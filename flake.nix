a17a031adf91792be5e65def9d5138ab56475f89
Fady Kuzman <fady.kuzman@pm.me>
2025-09-17 10:55:11 +0200

    Create dev env with devenv.sh with flakes

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              packages = with pkgs; [
+                nodejs
+                pkgs.nodePackages.tailwindcss
+                pkgs.nodePackages.prettier
+                pkgs.nodePackages.postcss
+                pkgs.nodePackages.autoprefixer
+                pkgs.nodePackages.eslint
+              ];
+
+              languages = {
+                typescript.enable = true;
+                javascript = {
+                  enable = true;
+                  pnpm = {
+                    enable = true;
+                    install.enable = true;
+                  };
+                };
+              };
+            }
+          ];
+        };
+      });
+  };
+}

7267a0cb3c4a337c4cdff91203a2769fb33a7472
Fady Kuzman <fady.kuzman@pm.me>
2025-09-20 08:20:21 +0200

    Implement proper spacing hierarchy

--- devfocus/src/components/Timer.tsx
+++ devfocus/src/components/Timer.tsx
@@ -5,28 +5,28 @@ import PlayPauseButton from "./PlayPauseButton";
 import ResetButton from "./ResetButton";
 
 const Timer = () => {
-  const { timeRemaining, isActive, startTimer, pauseTimer, resetTimer} =
-    useCountdownTimer();
+	const { timeRemaining, isActive, startTimer, pauseTimer, resetTimer } =
+		useCountdownTimer();
 
-  const handleToggle = () => {
-    isActive ? pauseTimer() : startTimer();
-  };
+	const handleToggle = () => {
+		isActive ? pauseTimer() : startTimer();
+	};
 
-  return (
-    <div
-      id="timer-container"
-      data-testid="timer-container"
-      className="flex flex-col items-center justify-center min-h-screen py-2"
-    >
-      <TimerDisplay timeRemainingInSeconds={timeRemaining} />
-      <div className="space-x-4">
-        <PlayPauseButton isActive={isActive} onToggle={handleToggle} />
-      </div>
-      <div className="space-x-4">
-        <ResetButton onClick={resetTimer}/>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			id="timer-container"
+			data-testid="timer-container"
+			className="flex flex-col items-center justify-center min-h-screen py-2"
+		>
+			<div className="mt-12">
+				<TimerDisplay timeRemainingInSeconds={timeRemaining} />
+			</div>
+			<div className="flex space-x-4">
+				<PlayPauseButton isActive={isActive} onToggle={handleToggle} />
+				<ResetButton onClick={resetTimer} />
+			</div>
+		</div>
+	);
 };
 
 export default Timer;
